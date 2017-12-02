69b895f2529f3a80e558fb9cc2c67355fa9d97c7
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2017-12-02 18:30:13 +0500

    Initial Xcode Project Initialized

---
+++ Customizer/Customizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Customizer
+//
+//  Created by Arsalan Wahid Asghar on 12/2/17.
+//  Copyright © 2017 Arsalan Wahid Asghar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
